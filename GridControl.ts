20b11f3521e7d15e4750c020f20a961fdf5a7b13
Paul Boon <gvr37.p.l.boon@gmail.com>
2017-09-10 13:36:16 +0200

    gridoncontrol reactor

--- GridControl.ts
+++ GridControl.ts
@@ -5,28 +5,44 @@
 /// <reference path="widgets/pointerWidget.ts" />
 /// <reference path="widgets/textWidget.ts" />
 
+var types = ['text','boolean','number','date','pointer','array']
 
 class GridControl{
+    tablebody: Element;
+    titlerow: Element;
+    searchrow: Element;
+    createlink: HTMLAnchorElement;
     filter: any;
     element: Element
     data
     definition:ObjDef
-    table
     onchange:EventSystem<any>
+    template:string = `
+        <div>
+            <a id="createlink">create</a>
+            <table id="table" style="width:100%;">
+                <thead>
+                    <tr id="titlerow"></tr>
+                    <tr id="searchrow"></tr>
+                </thead>
+                <tbody id="tablebody">
+
+                </tbody>
+            </table>
+        </div>
+    `
 
     constructor(element:Element, definition:ObjDef){
         this.onchange = new EventSystem()
         this.element = element;
         this.definition = definition
-        // this.filter = options.filter
-        
-        //create link
-        var createspan = document.createElement('span')
-        createspan.appendChild(string2html(`<a href="/#${this.definition.name}/new">create</a>`))
-        element.appendChild(createspan)
-        
-        this.table = string2html('<table id="table" style="width:100%;"></table>')
-        this.element.appendChild(this.table)
+
+        this.element.appendChild(string2html(this.template))
+        this.tablebody = this.element.querySelector('#tablebody') 
+        this.titlerow = this.element.querySelector('#titlerow')
+        this.searchrow = this.element.querySelector('#searchrow')
+        this.createlink = this.element.querySelector('#createlink') as HTMLAnchorElement
+        this.createlink.href = `/#${this.definition.name}/new`
 
         this.appendHeader()
         
@@ -37,102 +53,49 @@ class GridControl{
     }
 
     appendHeader(){
-        var nameHeader = document.createElement('tr')
-        this.table.appendChild(nameHeader)
         for(let attribute of this.definition.attributes){
-            if(attribute.type == 'array')continue
-            var td = document.createElement('td')
-            nameHeader.appendChild(td)
+            if(attribute.type == 'array')continue;
 
-            var columnTitle = new TextWidget(td)
+            //titlerow
+            var columnTitle = new TextWidget(createTableCell(this.titlerow))
             columnTitle.value.set(attribute.name)
+
+            //columnrow
+            var searchField = new TextWidget(createTableCell(this.searchrow))
         }
 
-
-        var searchHeader = document.createElement('tr')
-        this.table.appendChild(searchHeader)
-        for(let attribute of this.definition.attributes){
-            if(attribute.type == 'array')continue
-            var td = document.createElement('td')
-            searchHeader.appendChild(td)
-
-            var searchField = new TextWidget(td)
-        }
     }
 
     appendBody(data){
         for(let rows = 0; rows < data.length; rows++){
             var row = document.createElement('tr')
-            this.table.appendChild(row)
+            this.tablebody.appendChild(row)
+
             for(let attribute of this.definition.attributes){
                 if(attribute.type == 'array')continue
                 var td = document.createElement('td')
                 row.appendChild(td)
 
                 var widget = getWidget(attribute,td)
-                
                 widget.value.set(data[rows][attribute.name])
                 widget.value.onchange.listen((val) => {
                     data[rows][attribute.name] = val;
                     this.onchange.trigger(0,0)
                 })
-                
             }
 
             // save button
-            var savetd = document.createElement('td')
-            var savebtn = new Button(savetd, 'save',() => {
-                update(this.definition.name,data[rows]._id,data[rows],() => {
-                    
-                })
+            var savebtn = new Button(createTableCell(row), 'save',() => {
+                update(this.definition.name,data[rows]._id,data[rows],() => {})
             })
-            row.appendChild(savetd)
 
             // delete button
-            var deltd = document.createElement('td')
-            var deletebutton = new Button(deltd,'delete',() => {
-                del(this.definition.name,data[rows]._id,() => {
-
-                })
+            var deletebutton = new Button(createTableCell(row),'delete',() => {
+                del(this.definition.name,data[rows]._id,() => {})
             })
-            row.appendChild(deltd)
 
             //inspect link
-            var deltd = document.createElement('td')
-            deltd.appendChild(string2html(`<a href="/#${this.definition.name}/${data[rows]._id}">inspect</a>`))
-            row.appendChild(deltd)
+            createTableCell(row).appendChild(string2html(`<a href="/#${this.definition.name}/${data[rows]._id}">inspect</a>`))
         }
     }
 }
-
-function getWidget(attr:Attribute,element:HTMLElement):Widget<any>{
-    var widget:Widget<any>
-    switch (attr.type) {
-        case 'boolean':
-            widget = new BooleanWidget(element)
-            break;
-        case 'number':
-            widget = new NumberWidget(element,{})
-            break;
-        case 'date':
-            widget = new DateWidget(element)
-            break;
-        case 'pointer':
-            
-            
-
-            widget = new PointerWidget(element, attr, (val) => val._id, (val) => val.naam)
-            break;
-        default://text
-            widget = new TextWidget(element)
-            break;
-    }
-    return widget
-}
-
-function string2html(string):HTMLElement{
-    var div = document.createElement('div')
-    div.innerHTML = string;
-    return div.children[0] as HTMLElement;
-}
-var types = ['text','boolean','number','date','pointer','array']
--- utils.ts
+++ utils.ts
@@ -2,6 +2,34 @@ function mod(number, modulus){
     return ((number%modulus)+modulus)%modulus;
 }
 
+function getWidget(attr:Attribute,element:HTMLElement):Widget<any>{
+    var widget:Widget<any>
+    switch (attr.type) {
+        case 'boolean':
+            widget = new BooleanWidget(element)
+            break;
+        case 'number':
+            widget = new NumberWidget(element,{})
+            break;
+        case 'date':
+            widget = new DateWidget(element)
+            break;
+        case 'pointer':
+            widget = new PointerWidget(element, attr, (val) => val._id, (val) => val.naam)
+            break;
+        default://text
+            widget = new TextWidget(element)
+            break;
+    }
+    return widget
+}
+
+function string2html(string):HTMLElement{
+    var div = document.createElement('div')
+    div.innerHTML = string;
+    return div.children[0] as HTMLElement;
+}
+
 function getlist(pointertype,callback:(data) => void){
     fetch(`/api/${pointertype}`,{
         headers:{
@@ -69,4 +97,10 @@ function update(pointertype,id,data,callback:() => void){
         toastr.success('saved')
         callback()
     })
+}
+
+function createTableCell(row){
+    var td = document.createElement('td')
+    row.appendChild(td)
+    return td
 }
