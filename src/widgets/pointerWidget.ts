18fdd76e84de4591dd4fa15bf1049596aa039907
Paul Boon <gvr37.p.l.boon@gmail.com>
2017-09-20 12:22:07 +0200

    refactoring pointerwidget

--- src/widgets/pointerWidget.ts
+++ src/widgets/pointerWidget.ts
@@ -1,26 +1,19 @@
 /// <reference path="../widget.ts" />
 
 class PointerWidget extends Widget<string>{
+    optionsMap: Map<string, any>;
+    dropdowncontainer: HTMLElement;
     referencedObjectDropdownAttribute: Attribute;
     referencedObject: ObjDef;
     delbuttoncontainer: HTMLElement;
     attribute: pointerAttribute;
-    internalValue: Box<number>;
-    selectedindex: Box<number>
     value:Box<any>
-    onselect:EventSystem<any>
-    container:HTMLElement
-    input:HTMLInputElement
-    dropper:HTMLElement
+    dropdownWidget:DropDownWidget<any>
     link:HTMLAnchorElement
-    drops:Element[]
-    infoer:(val) => string
-    optionslist
     template:string = `
         <div style="display:flex; max-width:220px;">
-            <div id="container" style="position:relative; display:inline-block;"> 
-                <input class="form-control group-left" id="input" type="text"> 
-                <div id="dropper" class="dropper"></div> 
+            <div id="dropdowncontainer">
+
             </div>
             <span id="delbuttoncontainer"></span>
             <a class="btn btn-info group-right" id="link">-></a>
@@ -31,61 +24,57 @@ class PointerWidget extends Widget<string>{
         var that = this
         this.attribute = attribute
 
-        this.element.appendChild(string2html(this.template))
-        this.container = this.element.querySelector('#container') as HTMLElement
+        createAndAppend(this.element,this.template)
         this.link = this.element.querySelector('#link') as HTMLAnchorElement
-        this.input = this.element.querySelector('#input') as HTMLInputElement
-        this.dropper = this.element.querySelector('#dropper') as HTMLElement
         this.delbuttoncontainer = this.element.querySelector('#delbuttoncontainer') as HTMLElement
-        this.drops = []
+        this.dropdowncontainer = this.element.querySelector('#dropdowncontainer') as HTMLElement
 
         this.referencedObject = window.objectMap.get(attribute.pointerType)
         this.referencedObjectDropdownAttribute = window.attributeMap.get(this.referencedObject.dropdownAttribute)
 
         this.value.value = 0;
-        this.internalValue = new Box(0)
-        this.selectedindex = new Box<number>(0)
-        this.onselect = new EventSystem()
+        this.optionsMap = new Map<string,any>()
 
         getlist(this.referencedObject.name, (res) => {
-            that.optionslist = res
-            that.render(res)
+            // for(var obj of res){
+            //     that.optionsMap.set(obj._id,obj)
+            // }
+
+            that.dropdownWidget = new DropDownWidget<any>(that.dropdowncontainer,(val) => {
+                return that.getDisplayValue(val)
+            },res)
+
+            that.dropdownWidget.value.onchange.listen((val) => {
+                if(val == null){
+                    that.value.set(null)
+                }else{
+                    that.value.set(val._id)
+                }
+            })
         }, (error) => {
 
         })
 
         new Button(this.delbuttoncontainer,'X','btn btn-danger group-middle',() => {
-            that.internalValue.set(null)
+            that.dropdownWidget.value.set(null)
         })
 
-        this.internalValue.onchange.listen((val) => {
-            displayHasBeenSet = true
-            if(val == null){
-                that.input.value = 'nullptr'
-                that.value.set(null)
-            }else{
-                that.input.value = that.getDisplayValue(val)
-                that.value.set(val._id)
-            }
-        })
+
 
         var displayHasBeenSet = false
-        this.value.onchange.listen((pointer) => {//handle the case where val = undefined or null
-            //special case
-            //set display for first time set
+        this.value.onchange.listen((pointer) => {
             if(!displayHasBeenSet){
                 displayHasBeenSet = true
 
                 if(pointer == null){
-                    that.input.value = 'nullptr'
+                    // that.dropdownWidget.input.value = 'nullptr'
                 }else{
                     getobject(window.objectMap.get(attribute.pointerType).name ,pointer,(data) => {
                         if(data == null){
-                            that.input.value = 'null'
+                            that.dropdownWidget.input.value = 'null'
                         }else{
-                            that.input.value = that.getDisplayValue(data)
+                            that.dropdownWidget.input.value = that.getDisplayValue(data)
                         }
-                        
                     },(error) => {
                         
                     })
@@ -93,64 +82,6 @@ class PointerWidget extends Widget<string>{
             }
             that.link.href = `/#${attribute.name}/${pointer}`
         })
-
-        this.onselect.listen(() =>{
-            that.dropper.style.display = 'none'
-        })
-
-        this.selectedindex.onchange.listen((val, old) => {//for keydowns
-            that.internalValue.set(that.optionslist[val])
-
-            that.drops[val].classList.add('selected')
-            that.drops[old].classList.remove('selected')
-        })
-
-
-        
-
-        this.input.addEventListener('focus', () => {
-            that.dropper.style.display = 'block'
-        })
-
-        document.addEventListener('click', (e) => {
-            if(!that.container.contains(e.target as any)){
-                that.dropper.style.display = 'none'
-            }
-        })
-
-        this.input.addEventListener('keydown', (e) => {
-            if(e.keyCode == 38){
-                that.selectedindex.set(mod(that.selectedindex.get() - 1 ,that.optionslist.length))
-                that.dropper.style.display = 'block'
-            }
-
-            if(e.keyCode == 40){
-                that.selectedindex.set(mod(that.selectedindex.get() + 1 ,that.optionslist.length))
-                that.dropper.style.display = 'block'
-            }
-
-            if(e.keyCode == 13 || e.keyCode == 27){
-                that.dropper.style.display = 'none'
-            }
-        })
-
-        
-
-        
-    }
-
-    render(optionlist){
-        var that = this;
-        for(let option of optionlist){
-            var drop = string2html(`<div class="hovereffect">${this.getDisplayValue(option)}</div>`)
-            drop.addEventListener('click',() => {
-                this.internalValue.set(option)
-                this.onselect.trigger(option._id,0)
-            })
-            this.drops.push(drop)
-
-            this.dropper.appendChild(drop)
-        }
     }
 
     handleSetReadOnly(val: boolean) {
@@ -162,7 +93,11 @@ class PointerWidget extends Widget<string>{
         
     }
 
-    getDisplayValue(val){
-        return val[this.referencedObjectDropdownAttribute.name] || val._id
+    getDisplayValue(val):string{
+        if(val == null){
+            return 'nullptr'
+        }else{
+            return val[this.referencedObjectDropdownAttribute.name] || val._id
+        }
     }
 }
