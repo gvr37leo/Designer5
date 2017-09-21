01303f23a46305205ef3cf3a7c10d2c72ea5d323
Paul Boon <gvr37.p.l.boon@gmail.com>
2017-09-21 15:50:06 +0200

    checkbox refactor

--- src/widgets/booleanWidget.ts
+++ src/widgets/booleanWidget.ts
@@ -2,21 +2,37 @@
 
 
 class BooleanWidget extends Widget<boolean>{
+    slidertbn: HTMLElement;
+    slider: HTMLElement;
     inputel: HTMLInputElement;
-
+    template:string = `
+        <div class="slider">
+            <input class="sliderinput" type="checkbox"/>
+            <div id="sliderbtn" class="sliderbtn slideroff"></div>
+        </div>
+    `
 
     constructor(element:HTMLElement){
         super(element)
-        
-        this.inputel = string2html('<input type="checkbox"/>') as HTMLInputElement
-        this.element.appendChild(this.inputel)
-        
-        this.inputel.addEventListener('change',(e) => {
+        this.slider = createAndAppend(this.element,this.template)
+
+        this.inputel = this.element.querySelector("input") as HTMLInputElement
+        this.slidertbn = this.element.querySelector("#sliderbtn") as HTMLElement
+
+        this.slider.addEventListener('click',(e) => {
+            this.inputel.checked = !this.inputel.checked
             this.value.set(this.inputel.checked as any)
         })
 
         this.value.onchange.listen((val) => {
             this.inputel.checked = val
+            if (this.inputel.checked) {
+                this.slidertbn.classList.add('slideron')
+                this.slidertbn.classList.remove('slideroff')
+            } else {
+                this.slidertbn.classList.add('slideroff')
+                this.slidertbn.classList.remove('slideron')
+            }
         })
     }
 
